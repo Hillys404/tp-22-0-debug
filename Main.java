90d2cc5ce620b487fc10428d035e4aca9a531646
Jade <caussej@3il.fr>
2023-11-22 15:46:10 +0100

    Correction des erreurs

--- Main.java
+++ Main.java
@@ -2,23 +2,23 @@ public class Main {
     public static void main(String[] args) {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
 
         // Problème 2
-        double result = calculateValue(5, 0);
+        double result = calculateValue(5, 2);
         System.out.println("Result: " + result);
 
         // Problème 3
         int count = 10;
-        while (count > 0) {
+        while (count < 100) {
             System.out.println("Count: " + count);
             count += 2;
         }
 
         // Problème 4
-        String message = "Bonjour";
+        String message = "Bonjour à tosu !";
         System.out.println("Longueur du message : " + message.length());
         char character = message.charAt(10);
         System.out.println("Caractère à l'indice 10 : " + character);
@@ -26,10 +26,10 @@ public class Main {
         // Problème 5
         String str1 = "Hello";
         String str2 = new String("Hello");
-        System.out.println("Comparaison de chaînes : " + (str1 == str2));
+        System.out.println("Comparaison de chaînes : " + (str1.equals(str2)));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj = new Main();
         mainObj.method();
     }
 
